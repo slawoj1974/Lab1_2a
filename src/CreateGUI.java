797202ed688f7c3f4dfe0ff0ec279b369d02bd99
s.wojcik74 <s.wojcik74@wp.pl>
2023-10-28 12:37:25 +0200

    First

--- src/CreateGUI.java
+++ src/CreateGUI.java
@@ -3,7 +3,7 @@ import javax.swing.JFrame;
 public class CreateGUI
 {
     public static void main(String[] args)
-    {
+    {   // tworzymy nowe okno o zadanym rozmiarze
         JFrame frame = new JFrame();
         frame.setSize(640, 480);
         frame.setVisible(true);
